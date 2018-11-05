5b90fcfff5168b8b493b019849343dc3ea7303fc
Maryo Bezerra <maryobezerra@gmail.com>
2018-11-05 19:54:56 -0300

    Implementação dos recursos e serviços

---
+++ ProjetoTopicosEspeciais2/src/main/java/fvs/edu/br/topicos/repositories/EnderecoRepository.java
@@ -0,0 +1,12 @@
+package fvs.edu.br.topicos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fvs.edu.br.topicos.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+
+}
